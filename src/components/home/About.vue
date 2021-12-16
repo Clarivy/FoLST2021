0b3616dd50e9415bc39621f9abff339b08f62df4
Clarivy <zqc.sc.hn@gmail.com>
2021-12-16 10:21:45 +0800

    Update some wrong info

--- src/components/home/About.vue
+++ src/components/home/About.vue
@@ -19,7 +19,7 @@
       </v-col>
       <v-col md="6" cols="12" class="py-6" >
         <p class="google-font" style="font-size: 110%;line-height: 1.22;font-weight:300;line-height: 1.6;">
-          Officially approved to open on 30 September 2013 by China’s Ministry of Education, ShanghaiTech University is a small-scale research university of academic excellence jointly established by Shanghai Municipal Government and Chinese Academy of Sciences. SLST at ShanghaiTech aims to develop into a world-class, research-oriented school in life science and technology, drawing strength not only from its outstanding full-time faculty but also from five world-class biomedical research institutions (iHuman, NCPSS, BME school, SCRaTC and SIAIS) at or around ShanghaiTech as well as from selected faculty members jointly appointed with institutes of Chinese Academy of Sciences in Shanghai who conduct world-class research.
+          Officially approved to open on 30 September 2013 by China’s Ministry of Education, ShanghaiTech University is a small-scale research university of academic excellence jointly established by Shanghai Municipal Government and Chinese Academy of Sciences. SLST at ShanghaiTech aims to develop into a world-class, research-oriented school in life science and technology, drawing strength not only from its outstanding full-time faculty but also from five world-class biomedical research institutions (iHuman, NCPSS, BME School, SCRaTC and SIAIS) at or around ShanghaiTech as well as from selected faculty members jointly appointed with institutes of Chinese Academy of Sciences in Shanghai who conduct world-class research.
         </p>
 
       </v-col>
